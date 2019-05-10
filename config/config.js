976919009d87ca8e50a9575d31d2360706d796b5
Alejandro Vallejo <alejandro.visionarios@vis.com.co>
2019-05-10 17:44:01 -0500

    Continuacion.

--- config/config.js
+++ config/config.js
@@ -1,1 +1,9 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+
+// ===========================
+// Google
+// ===========================
+
+
+module.exports.CLIENT_ID = '479832660394-sgs7qt0fuar53n5vhcq7frfe2n6kthd3.apps.googleusercontent.com';
