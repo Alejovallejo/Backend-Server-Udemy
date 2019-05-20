5eb2168d13f6c02b99409acafd3661a4d5b516d8
Alejandro Vallejo <alejandro.visionarios@vis.com.co>
2019-05-20 17:21:12 -0500

    Backend Server modificado, y estable.

--- app.js
+++ app.js
@@ -7,18 +7,19 @@ var bodyParser = require('body-parser');
 var app = express();
 
 
-// cors
+// CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     next();
-  });
-  
+});
+
+
 // Body Parser
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 
 // Importar rutas
--- config/config.js
+++ config/config.js
@@ -1,8 +1,5 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
 
 // Google
-
-	
-
 module.exports.GOOGLE_CLIENT_ID = '479832660394-sgs7qt0fuar53n5vhcq7frfe2n6kthd3.apps.googleusercontent.com';
 module.exports.GOOGLE_SECRET = 'juJIzOXM7bZxH3qzKg3dU7GS';
